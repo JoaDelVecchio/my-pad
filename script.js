e06a3c2f3d822f1eb665daad34a5d984befe542c
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-06-26 19:57:37 -0300

    Make the whole body of the HTML in js
    - Make header, main and footer objects directly on JS and add content to it.
    - Reorganize the code.

--- script.js
+++ script.js
@@ -1,102 +1,140 @@
-// FUNCTIONS
-function makeGrid() {
-  gridContainer.innerHTML = "";
-  gridContainer.style.cssText = `flex-direction:column;display:flex;align-items:center`;
-  // FILAS
-  for (let i = 0; i < newSize; i++) {
-    let row = document.createElement("div");
-    row.style.display = "flex";
-    row.style.flexDirection = "row";
-    gridContainer.appendChild(row);
+document.addEventListener("DOMContentLoaded", () => {
+  //VARIABLES && INICIALIZATION
+  const header = document.createElement("header");
+  document.body.prepend(header);
 
-    // COLUMNAS
-    for (let j = 0; j < newSize; j++) {
-      let col = gridContainer.appendChild(document.createElement("div"));
-      col.style.cssText = `height:${400 / newSize}px;width:${
-        400 / newSize
-      }px;border:black solid 1px;background-color:white;transition:1s`;
-      col.addEventListener("mouseover", function () {
-        if (this.style.backgroundColor === "white") {
-          this.style.backgroundColor = currentColor;
-        } else {
-          this.style.backgroundColor = "white";
-        }
+  const main = document.createElement("main");
+  document.body.appendChild(main);
+
+  const buttonsContainer = document.createElement("div");
+  main.appendChild(buttonsContainer);
+
+  const gridContainer = document.createElement("div");
+  main.appendChild(gridContainer);
+
+  const footer = document.createElement("footer");
+  document.body.appendChild(footer);
+
+  let currentColor = "black";
+  let newSize = 16;
+  let headerText;
+
+  function enlargeFont(event) {
+    event.target.style.fontSize = "18px";
+  }
+
+  function decrementFont(event) {
+    event.target.style.fontSize = "16px";
+  }
+
+  function addButton(
+    container,
+    text,
+    onClickFunction = null,
+    colorPicker = false
+  ) {
+    let newButton = document.createElement("button");
+    newButton.style.cssText =
+      "width:140px;height:50px;font-size:16px;font-weight:bold;color:white;border-radius:5px;margin-right:10px;background-color:#000080";
+    newButton.textContent = text;
+
+    if (onClickFunction !== null) {
+      newButton.addEventListener("click", onClickFunction);
+    }
+
+    newButton.addEventListener("mouseover", enlargeFont);
+    newButton.addEventListener("mouseout", decrementFont);
+
+    if (colorPicker) {
+      let input = document.createElement("input");
+      input.type = "color";
+      input.style.position = "absolute";
+      input.style.marginLeft = "80px";
+      input.style.marginTop = "30px";
+      input.style.opacity = 0;
+      input.addEventListener("change", () => {
+        currentColor = input.value;
       });
-      row.appendChild(col);
+      newButton.appendChild(input);
+      newButton.addEventListener("click", () => input.click());
+    }
+
+    container.appendChild(newButton);
+    return newButton;
+  }
+
+  function makeGrid() {
+    gridContainer.innerHTML = "";
+    gridContainer.style.cssText = `flex-direction:column;display:flex;align-items:center`;
+
+    for (let i = 0; i < newSize; i++) {
+      let row = document.createElement("div");
+      row.style.display = "flex";
+      row.style.flexDirection = "row";
+      gridContainer.appendChild(row);
+
+      for (let j = 0; j < newSize; j++) {
+        let col = document.createElement("div");
+        col.style.cssText = `height:${400 / newSize}px;width:${
+          400 / newSize
+        }px;border:black solid 1px;background-color:white;transition:1s`;
+        col.addEventListener("mouseover", function () {
+          this.style.backgroundColor =
+            this.style.backgroundColor === "white" ? currentColor : "white";
+        });
+        row.appendChild(col);
+      }
     }
   }
-}
 
-function changeSize() {
-
-  let input = prompt("Change size to:");
-  if (wasCancelled(input)) {
-    return;
+  function wasCancelled(input) {
+    return input === null;
   }
 
-  while (isNaN(parseInt(input)) || parseInt(input) > 100 || parseInt(input) < 1 ) {
-      input = prompt("Incorret number, try again!, Change size to:");
-      
+  function changeSize() {
+    let input = prompt("Change size to:");
+    if (wasCancelled(input)) {
+      return;
+    }
+
+    while (
+      isNaN(parseInt(input)) ||
+      parseInt(input) > 100 ||
+      parseInt(input) < 1
+    ) {
+      input = prompt("Incorrect number, try again! Change size to:");
       if (wasCancelled(input)) {
-          return;
-        }
-        
-        newSize = parseInt(input);
+        return;
+      }
     }
 
-  newSize = parseInt(input);
-  makeGrid(newSize);
-}
-
-function wasCancelled(input) {
-  if (input === null) {
-    return true;
+    newSize = parseInt(input);
+    makeGrid();
   }
-  return false;
-}
 
-function addButton(container, text, onClickFunction = null, colorPicker = false ) {
-  newButton = document.createElement("button");
-  newButton.style.cssText =
-    "width:120px;height:50px;font-size:16px;font-weight:bold;color:white;border-radius:5px";
-  newButton.textContent = text;
-  newButton.addEventListener("click", onClickFunction);
-  if (colorPicker !== false)
-    {
-    let input = newButton.appendChild(document.createElement("input"));
-    input.type = "color";
-    input.addEventListener("change", ()=> currentColor = input.value)
-    newButton.appendChild(input);
-    }
-  container.appendChild(newButton);
-  return newButton;
-}
 
-// VARIABLES
+  //HEADER
+  header.style.cssText = "width:100%;height:10vh"
+  headerText = document.createElement("p");
+  headerText.textContent = "My Pad"
+  headerText.style.cssText = "display:flex;justify-content:center;align-items:center;font-size:50px;"
+  header.appendChild(headerText)
+  //MAIN
+  main.style.cssText = "width:100%;display:flex;flex-direction:column;justify-content:center;align-items:center;gap:40px;padding:30px";
 
-document
-  .querySelectorAll("*")
-  .forEach(
-    (element) =>
-      (element.style.cssText = "box-sizing:border-box;margin: 0;padding: 0")
-  );
+  buttonsContainer.style.cssText = "display:flex;justify-content:center;gap:20px;"
+  addButton(buttonsContainer, "Change Size", changeSize);
+  addButton(buttonsContainer, "Clear Pad", () => makeGrid(newSize));
+  let changeColorBtn = addButton(buttonsContainer, "Change Color", null, true);
+  changeColorBtn.style.width = "150px";
 
-let newSize = 16; //16X16 DEFAULT SIZE
-let currentColor = "black";
-const gridContainer = document
-  .querySelector("main")
-  .appendChild(document.createElement("div"));
+  makeGrid();
 
-// HEADER
+  //FOOTER
+  footer.style.cssText = "width:100%;height:5vh"
+  footerText = document.createElement("p");
+  footerText.textContent = "made by JoaDelVecchio"
+  footerText.style.cssText = "display:flex;justify-content:center;align-items:center;font-size:20px;"
+  footer.appendChild(footerText)
 
-header = document.querySelector("header");
-header.style.cssText = "height:100px;padding:10px;display:flex";
-
-addButton(header, "Change Size", changeSize);
-addButton(header, "Clear Pad", makeGrid);
-let changeColorBtn = addButton(header, "Change Color",null,true);
-changeColorBtn.style.width = "150px"
-
-// MAIN
-
-makeGrid(newSize);
+});
