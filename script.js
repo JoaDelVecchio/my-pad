67c2909e15bf7b34d7e40b86e6e7632285647c29
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-06-26 14:59:46 -0300

    Add "Change color" button and it's functionality
    - It was created so the user can choose the color
    to paint the grid

--- script.js
+++ script.js
@@ -14,10 +14,11 @@ function makeGrid() {
       let col = gridContainer.appendChild(document.createElement("div"));
       col.style.cssText = `height:${400 / newSize}px;width:${
         400 / newSize
-      }px;border:black solid 1px;background-color:white`;
+      }px;border:black solid 1px;background-color:white;transition:3s`;
       col.addEventListener("mouseover", function () {
         if (this.style.backgroundColor === "white") {
-          this.style.backgroundColor = "black";
+          this.style.backgroundColor = currentColor;
+          setTimeout(()=>this.style.backgroundColor = "white",1500);
         } else {
           this.style.backgroundColor = "white";
         }
@@ -28,22 +29,23 @@ function makeGrid() {
 }
 
 function changeSize() {
+
   let input = prompt("Change size to:");
   if (wasCancelled(input)) {
     return;
   }
 
-  newSize = parseInt(input);
-
-  while (isNaN(newSize) || newSize > 100) {
-    input = prompt("Incorret number, try again!, Change size to:");
-
-    if (wasCancelled(input)) {
-      return;
+  while (isNaN(parseInt(input)) || parseInt(input) > 100 || parseInt(input) < 1 ) {
+      input = prompt("Incorret number, try again!, Change size to:");
+      
+      if (wasCancelled(input)) {
+          return;
+        }
+        
+        newSize = parseInt(input);
     }
 
-    newSize = parseInt(input);
-  }
+  newSize = parseInt(input);
   makeGrid(newSize);
 }
 
@@ -63,15 +65,16 @@ function addButton(container, text, onClickFunction) {
   container.appendChild(newButton);
 }
 
+// VARIABLES
 document
   .querySelectorAll("*")
   .forEach(
     (element) =>
       (element.style.cssText = "box-sizing:border-box;margin: 0;padding: 0")
   );
-// VARIABLES
 
-let newSize = 4; //4X4
+let newSize = 16; //16X16 DEFAULT SIZE
+let currentColor = "black";
 const gridContainer = document
   .querySelector("main")
   .appendChild(document.createElement("div"));
@@ -82,7 +85,15 @@ header.style.cssText = "height:100px;padding:10px;display:flex";
 
 addButton(header, "Change Size", changeSize);
 addButton(header, "Clear Pad", makeGrid);
-//addButton(header,"Change Color",changeColor);
+
+let colorPickerBtn = document.createElement("button");
+colorPickerBtn.textContent = "Change Color";
+colorPickerBtn.style.cssText ="width:150px;height:50px;font-size:16px;font-weight:bold;color:white;border-radius:5px";
+let colorPickerInput = document.createElement("input");
+colorPickerInput.type = "color";
+colorPickerBtn.appendChild(colorPickerInput);
+colorPickerInput.addEventListener("change", ()=> currentColor = colorPickerInput.value)
+header.appendChild(colorPickerBtn)
 
 // MAIN
 
